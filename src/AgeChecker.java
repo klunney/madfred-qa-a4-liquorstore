bde4f6f9beb6e5aaad6ffeb628d603b0c5a8ed79
Katie <k_lunney1@hotmail.com>
2013-09-13 12:14:55 -0400

    Here is ass ignment 4

--- src/AgeChecker.java
+++ src/AgeChecker.java
@@ -1,15 +1,35 @@
+import javax.swing.JOptionPane;
 
 public class AgeChecker {
-
+	//asks user for input//
 	public static void main(String[] args) {
-		/*---------------------------------
-		 * In this file, you will recreate 
-		 * the age verification program we
-		 * created in class. When you're
-		 * finished, delete these instructions
-		 * before submitting.
-		 ---------------------------------*/
+		String firstName = JOptionPane.showInputDialog(null, "What is your first name?");
+		String lastName = JOptionPane.showInputDialog(null, "What is your last name?");
+		int yearOfBirth = Integer.parseInt(JOptionPane.showInputDialog(null,"What year were you born?"));
+	//verify and concatenate ages/names
+		boolean oldEnough = verifyLegalAge(yearOfBirth);
+		String fullName = concatenateNames(firstName, lastName);
+	//outputs string depending on the result
+		if(oldEnough == true) {
+			JOptionPane.showMessageDialog(null, fullName + ", you may proceed with your shopping:");
+		} else {
+			JOptionPane.showMessageDialog(null, fullName + ", you're not old enough, try disney.com instead!");
+		}
 
 	}
-
+	//takes an int and returns a boolean value if 19 or over//
+	public static boolean verifyLegalAge(int yob) {
+		boolean legalAgeStatus = (2013-yob) >= 19;
+		return legalAgeStatus;
+	}
+	//uses two string and makes one//
+	public static String concatenateNames(String fn, String ln) {
+		String fullName = fn + " " + ln;
+		return fullName;
+	}
 }
+
+
+
+
+
